2be081fd6b6e53e7890de5eaa943222527519443
Johan Steven Bueno Rojas <johanstevenbr@ufps.edu.co>
2026-09-11 07:00:10 -0500

    Se realizo actualizacion para guardar estudiantes, usando el JS

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+let estudiantes = [];
 const formulario = document.querySelector("#formEstudiante");
 
 formulario.addEventListener("submit", function(event) {
@@ -9,3 +10,10 @@ const nombre = document.querySelector("#nombre").value;
 const correo = document.querySelector("#correo").value;
 const programa = document.querySelector("#programa").value;
 
+const estudiante = {
+    nombre,
+    correo,
+    programa
+};
+
+estudiantes.push(estudiante);
