af949c0ecf88a6e562fe6a235c6d6fbe67e0d8a2
Johan Steven Bueno Rojas <johanstevenbr@ufps.edu.co>
2026-09-11 07:17:27 -0500

    Añadidas las funciones para editar y eliminar estudiantes

--- js/app.js
+++ js/app.js
@@ -1,19 +1,82 @@
 let estudiantes = [];
+let estudianteEditando = null;
 const formulario = document.querySelector("#formEstudiante");
+
 
 formulario.addEventListener("submit", function(event) {
     event.preventDefault();
+
+    const nombre = document.querySelector("#nombre").value;
+    const correo = document.querySelector("#correo").value;
+    const programa = document.querySelector("#programa").value;
+
+    if (estudianteEditando === null) {
+        const estudiante = {
+            id: Date.now(),
+            nombre,
+            correo,
+            programa
+        };
+
+        estudiantes.push(estudiante);
+    } else {
+        const estudiante = estudiantes.find(
+            estudiante => estudiante.id === estudianteEditando
+        );
+
+        estudiante.nombre = nombre;
+        estudiante.correo = correo;
+        estudiante.programa = programa;
+        estudianteEditando = null;
+    }
+
+    mostrarEstudiantes();
+    formulario.reset();
     console.log("Formulario enviado");
 });
 
-const nombre = document.querySelector("#nombre").value;
-const correo = document.querySelector("#correo").value;
-const programa = document.querySelector("#programa").value;
+function mostrarEstudiantes(lista = estudiantes) {
 
-const estudiante = {
-    nombre,
-    correo,
-    programa
-};
+    const tabla = document.querySelector("#tablaEstudiantes");
 
-estudiantes.push(estudiante);
+    tabla.innerHTML = "";
+
+    lista.forEach(estudiante => {
+
+        tabla.innerHTML += `
+            <tr>
+                <td>${estudiante.nombre}</td>
+                <td>${estudiante.correo}</td>
+                <td>${estudiante.programa}</td>
+                <td>
+                    <button onclick="editarEstudiante(${estudiante.id})">Editar</button>
+                    <button onclick="eliminarEstudiante(${estudiante.id})">Eliminar</button>
+                </td>
+            </tr>
+        `;
+    });
+}
+
+function eliminarEstudiante(id) {
+
+    estudiantes = estudiantes.filter(
+        estudiante => estudiante.id !== id
+    );
+
+    mostrarEstudiantes();
+}
+
+function editarEstudiante(id) {
+
+    const estudiante = estudiantes.find(
+        estudiante => estudiante.id === id
+    );
+
+    document.querySelector("#nombre").value = estudiante.nombre;
+    document.querySelector("#correo").value = estudiante.correo;
+    document.querySelector("#programa").value = estudiante.programa;
+
+    estudianteEditando = id;
+}
+
+mostrarEstudiantes();
